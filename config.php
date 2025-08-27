3947b5899f93d6a7a523b5bc18b5c3469ec4edc7
sabafreelancingwali <sabakfw@gmail.com>
2025-08-27 18:34:57 +0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+$host = 'localhost';
+$dbname = 'dbsubd42rr081b';
+$username = 'upknjbhg8vsv8';
+$password = 'yz88ljtio3sf';
+ 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
